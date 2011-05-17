52d35c1d68d44e5240dbbd8538a0eb30839d6647
Alex Clark <aclark@aclark.net>
2011-05-17 14:27:09 -0400

    Add zcml to make my.package "show up"

--- src/my.package/setup.py
+++ src/my.package/setup.py
@@ -1,5 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup (
     name='my.package',
+    packages=find_packages(),
+    namespace_packages=['my'],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone 
+    """
 )
